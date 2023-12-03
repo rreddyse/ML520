ca3c10f2b3c752ef8230365f813aaaadd3165c91
rreddyse <148163112+rreddyse@users.noreply.github.com>
2023-12-03 15:49:06 -0600

    Update index.js

--- DeployProduction/ChurnPrediction/NN/index.js
+++ DeployProduction/ChurnPrediction/NN/index.js
@@ -1,21 +1,22 @@
 async function runExample() {
-  var x = new Float32Array(1, 14);
+ 
 
   var x = [];
-  x[0] = parseFloat(document.getElementById('box1').value);
-  x[1] = parseFloat(document.getElementById('box2').value);
-  x[2] = parseFloat(document.getElementById('box3').value);
-  x[3] = parseFloat(document.getElementById('box4').value);
-  x[4] = parseFloat(document.getElementById('box5').value);
-  x[5] = parseFloat(document.getElementById('box6').value);
-  x[6] = parseFloat(document.getElementById('box7').value);
-  x[7] = parseFloat(document.getElementById('box8').value);
-  x[8] = parseFloat(document.getElementById('box9').value);
-  x[9] = parseFloat(document.getElementById('box10').value);
-  x[10] = parseFloat(document.getElementById('box11').value);
-  x[11] = parseFloat(document.getElementById('box12').value);
-  x[12] = parseFloat(document.getElementById('box13').value);
-  x[13] = parseFloat(document.getElementById('box14').value);
+ x[0] =document.getElementById('box1').value;
+  x[1] =document.getElementById('box2').value;
+  x[2] =document.getElementById('box3').value;
+  x[3] =document.getElementById('box4').value;
+  x[4] =document.getElementById('box5').value;
+  x[5] =document.getElementById('box6').value;
+  x[6] =document.getElementById('box7').value;
+  x[7] =document.getElementById('box8').value;
+  x[8] =document.getElementById('box9').value;
+  x[9] =document.getElementById('box10').value;
+  x[10] =document.getElementById('box11').value;
+  x[11] =document.getElementById('box12').value;
+  x[12] =document.getElementById('box13').value;
+  x[13] =document.getElementById('box14').value;
+  
 
   let tensorX = new onnx.Tensor(x, 'float32', [1, 14]);
 
