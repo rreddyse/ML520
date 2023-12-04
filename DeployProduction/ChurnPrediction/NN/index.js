5d5f53b970fff4be21dec65df8ee2de8edbfbd5d
rreddyse <148163112+rreddyse@users.noreply.github.com>
2023-12-04 10:09:00 -0600

    Update index.js

--- DeployProduction/ChurnPrediction/NN/index.js
+++ DeployProduction/ChurnPrediction/NN/index.js
@@ -1,23 +1,9 @@
 async function runExample() {
   // Retrieve user inputs from HTML input fields
   var x = [];
-  x[0] = parseFloat(document.getElementById('box1').value);
-  x[1] = parseFloat(document.getElementById('box2').value);
-  x[2] = parseFloat(document.getElementById('box3').value);
-  x[3] = parseFloat(document.getElementById('box4').value);
-  x[4] = parseFloat(document.getElementById('box5').value);
-  x[5] = parseFloat(document.getElementById('box6').value);
-  x[6] = parseFloat(document.getElementById('box7').value);
-  x[7] = parseFloat(document.getElementById('box8').value);
-  x[8] = parseFloat(document.getElementById('box9').value);
-  x[9] = parseFloat(document.getElementById('box10').value);
-  x[10] = parseFloat(document.getElementById('box11').value);
-  x[11] = parseFloat(document.getElementById('box12').value);
-  x[12] = parseFloat(document.getElementById('box13').value);
-  x[13] = parseFloat(document.getElementById('box14').value);
-  x[14] = parseFloat(document.getElementById('box15').value);
-  x[15] = parseFloat(document.getElementById('box16').value);
-  x[16] = parseFloat(document.getElementById('box17').value);
+  for (let i = 1; i <= 17; i++) {
+    x.push(parseFloat(document.getElementById('box' + i).value));
+  }
 
   // Create an ONNX Tensor with user inputs
   let tensorX = new onnx.Tensor(x, 'float32', [1, 17]);
@@ -26,11 +12,11 @@ async function runExample() {
 
   await session.loadModel("churn_prediction.onnx");
   let outputMap = await session.run([tensorX]);
-  let outputData = outputMap.get('output1');
+  let outputData = outputMap.values().next().value;
 
   // Round the output value to the nearest integer
   let roundedOutput = Math.round(outputData.data[0]);
-  console.log('Model exported to ONNX:', onnxPath);
+
   // Display predictions on the web page
   let predictions = document.getElementById('predictions');
   predictions.innerHTML = `
