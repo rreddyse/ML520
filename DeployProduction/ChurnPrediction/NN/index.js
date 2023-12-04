c84506a12e9614c653af1e6d7ea105ebeaa7cce0
rreddyse <148163112+rreddyse@users.noreply.github.com>
2023-12-03 23:40:31 -0600

    Update index.js

--- DeployProduction/ChurnPrediction/NN/index.js
+++ DeployProduction/ChurnPrediction/NN/index.js
@@ -1,44 +1,45 @@
 async function runExample() {
- 
-
+  // Retrieve user inputs from HTML input fields
   var x = [];
-  x[0] =document.getElementById('box1').value;
-  x[1] =document.getElementById('box2').value;
-  x[2] =document.getElementById('box3').value;
-  x[3] =document.getElementById('box4').value;
-  x[4] =document.getElementById('box5').value;
-  x[5] =document.getElementById('box6').value;
-  x[6] =document.getElementById('box7').value;
-  x[7] =document.getElementById('box8').value;
-  x[8] =document.getElementById('box9').value;
-  x[9] =document.getElementById('box10').value;
-  x[10] =document.getElementById('box11').value;
-  x[11] =document.getElementById('box12').value;
-  x[12] =document.getElementById('box13').value;
-  x[13] =document.getElementById('box14').value;
-  x[14] =document.getElementById('box15').value;
-  x[15] =document.getElementById('box16').value;
-  x[16] =document.getElementById('box17').value;
+  x[0] = parseFloat(document.getElementById('box1').value);
+  x[1] = parseFloat(document.getElementById('box2').value);
+  x[2] = parseFloat(document.getElementById('box3').value);
+  x[3] = parseFloat(document.getElementById('box4').value);
+  x[4] = parseFloat(document.getElementById('box5').value);
+  x[5] = parseFloat(document.getElementById('box6').value);
+  x[6] = parseFloat(document.getElementById('box7').value);
+  x[7] = parseFloat(document.getElementById('box8').value);
+  x[8] = parseFloat(document.getElementById('box9').value);
+  x[9] = parseFloat(document.getElementById('box10').value);
+  x[10] = parseFloat(document.getElementById('box11').value);
+  x[11] = parseFloat(document.getElementById('box12').value);
+  x[12] = parseFloat(document.getElementById('box13').value);
+  x[13] = parseFloat(document.getElementById('box14').value);
+  x[14] = parseFloat(document.getElementById('box15').value);
+  x[15] = parseFloat(document.getElementById('box16').value);
+  x[16] = parseFloat(document.getElementById('box17').value);
 
-
+  // Create an ONNX Tensor with user inputs
   let tensorX = new onnx.Tensor(x, 'float32', [1, 17]);
 
-let session = new onnx.InferenceSession();
+  let session = new onnx.InferenceSession();
 
-await session.loadModel("churn_prediction.onnx");
-let outputMap = await session.run([tensorX]);
-let outputData = outputMap.get('output1');
+  await session.loadModel("churn_prediction.onnx");
+  let outputMap = await session.run([tensorX]);
+  let outputData = outputMap.get('output1');
 
-// Round the output value to the nearest integer
-let roundedOutput = Math.round(outputData.data[0]);
+  // Round the output value to the nearest integer
+  let roundedOutput = Math.round(outputData.data[0]);
 
-let predictions = document.getElementById('predictions');
-predictions.innerHTML = ` <hr> Got an output tensor with value: <br />
-  <table>
+  // Display predictions on the web page
+  let predictions = document.getElementById('predictions');
+  predictions.innerHTML = `
+    <hr> Churn Prediction Result: <br />
+    <table>
       <tr>
-          <td>Customer exited or not</td>
-          <td id="td0"> ${roundedOutput} </td>
+        <td>Customer exited or not</td>
+        <td id="result">${roundedOutput}</td>
       </tr>
-  </table>`
- ;
+    </table>
+  `;
 }
